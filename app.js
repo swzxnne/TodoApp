b8204a93fe2e464ce6e6fe63d14c4d892033aed8
Suzanne <70836805+swzxnne@users.noreply.github.com>
2026-03-03 18:01:09 +0530

    completed

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const todoForm = document.querySelector('form');
 const todoInput = document.getElementById('todo-input');
 const todoListUL = document.getElementById('todo-list');
 
-let allTodos = []; // all todos in an array
+let allTodos = getTodos();
+updateTodoList();
 
 todoForm.addEventListener('submit', function(e){  //taking input
   e.preventDefault(); // will not load autimatically
@@ -11,9 +12,14 @@ todoForm.addEventListener('submit', function(e){  //taking input
 function addTodo(){
     const todoText = todoInput.value.trim(); //variable = assign a value
     if(todoText.length > 0){
+        const todoObject = {
+            text: todoText,
+            completed: false
+        }
 
-         allTodos.push(todoText); //adding todos to array
+         allTodos.push(todoObject); //adding todos to array
          updateTodoList();
+         saveTodos();
          todoInput.value =""; //setting toso to an empty string
     }
    
@@ -29,6 +35,7 @@ function updateTodoList(){
 function createTodoItem(todo, todoIndex){
     const todoId = "todo-" + todoIndex;
     const todoLI = document.createElement("li");
+    const todoText = todo.text;
     todoLI.className = "todo";
     todoLI.innerHTML =`
     <input type="checkbox" id="${todoId}">
@@ -36,11 +43,36 @@ function createTodoItem(todo, todoIndex){
                     <svg fill = "transparent"  xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#1f1f1f"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg>
                 </label>
                 <label for="${todoId}" class="todo-text">
-                    ${todo}
+                    ${todoText}
                 </label>
                 <button class="delete-button">
                     <svg fill ="var(--secondary-color)" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#1f1f1f"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
                 </button>`
-    return todoLI;
+   
+
+    const deleteButton = todoLI.querySelector(".delete-button");
+    deleteButton.addEventListener("click", ()=>{
+        deleteTodoItem(todoIndex);
+    })
+    const checkbox = todoLI.querySelector("input");
+    checkbox.addEventListener("change", ()=>{
+        allTodos[todoIndex].completed = checkbox.checked;
+        saveTodos();
+    })
+    checkbox.checked = todo.completed;
+     return todoLI;
+}
+function deleteTodoItem(todoIndex){
+    allTodos = allTodos.filter((_, i)=> i !== todoIndex);
+    saveTodos();
+    updateTodoList();
 
 }
+function saveTodos(){
+    const todoJson = JSON.stringify(allTodos);
+    localStorage.setItem("todos", todoJson);
+}
+ function getTodos(){
+     const todos = localStorage.getItem("todos") || null;
+    return JSON.parse(todos);
+ }
